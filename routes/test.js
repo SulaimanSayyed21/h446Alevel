7da00451a9ea7c75e1f1721a93b32899ca8bb61a
SulaimanSayyed21 <sullisayyed21@outlook.com>
2024-05-01 15:12:22 +0100

    extra comments deleted

--- routes/test.js
+++ routes/test.js
@@ -19,21 +19,6 @@ router.get('/test', function (req, res, next) {
     res.redirect(302, 'login');
    }
   });
-
-
-
-//   if (!username && source !== 'guest') {
-//       console.log('User does not exist');
-//       res.redirect(302, 'login');
-//   } else if (source === 'guest') {
-//       console.log('Guest logged in');
-//       res.render('test', { title: 'Test page', username: source, showLogout: false });
-//   } else {
-//       res.render('test', { title: 'Test page', username: username, showLogout: true });
-//   } 
-// });
-
-/* Delegate the responsibility to the login controller*/
 router.post('/test', testController.handleTestRequest);
 module.exports = router;
 
